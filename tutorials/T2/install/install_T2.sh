cb3ade31619d8abb7fdfcd58001cb5d23c51f518
Juraj Ovčar <juraj.ovcar@gmail.com>
2025-05-23 13:54:56 +0200

    tutorials/T2: Add installation script
    The installation script can be run stand-alone, but we
    will add a script that (hopefully) installs everything.
    
    xtb has been moved from the requirements_T2.txt to installation
    through the conda-forge channel.

---
+++ tutorials/T2/install/install_T2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if CONDA_PREFIX is set
+
+if [ -z "${CONDA_PREFIX}" ]; then
+	echo "The CONDA_PREFIX variable is empty."
+	echo "Possibly, the conda base environment is not activated or conda is not installed."
+	echo "Exiting."
+	exit 1
+fi
+
+# Check if currently in the base environment
+
+if [ "$CONDA_DEFAULT_ENV" != "base" ]; then
+	echo "This script should be run in the base conda environment."
+	echo "Exiting."
+	exit 1
+fi
+
+# Remember the prefix of the base environment
+
+CONDA_BASE_PREFIX=$CONDA_PREFIX
+
+# Check if the script is being run from ../../../install_environments.sh
+# or from the same directory it is located in
+
+if [ -z "${TUTORIALS}" ]; then
+
+	T_PATH=$PWD
+
+fi
+
+### Install
+
+conda create -y -n T2 -c conda-forge -c anaconda ipykernel xtb
+
+source $CONDA_BASE_PREFIX/bin/activate T2
+python -m ipykernel install --user --name=T2
+python -m pip install -r $T_PATH/requirements_T2.txt
+
+source $CONDA_BASE_PREFIX/bin/activate base
