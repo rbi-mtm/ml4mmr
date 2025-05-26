fcdd11ec29132532986039b5be98ef87f8fc152d
Juraj Ovčar <juraj.ovcar@gmail.com>
2025-05-26 09:32:05 +0200

    INSTALL: Allow for system-wide installation
    System-wide installation allows for all the environments/kernels to be
    installed only once and are as such available to all users.
    
    Documentation:
    https://ipython.readthedocs.io/en/stable/install/kernel_install.html#kernels-for-different-environments

--- install/T2/install.sh
+++ install/T2/install.sh
@@ -1,12 +1,79 @@
 #!/bin/bash
 
 #
-# Usage:
+# (1) User-wide installation:
 #
-# in a conda base environment, run:
+# 	in a conda base environment, run:
 #
-# source install.sh
+# 		source install.sh
 #
+# (2) System-wide installation
+#
+# 		sudo -s
+#
+# 		# activate conda base environment
+# 		source /path/to/conda/activate base
+#
+# 		source install.sh -p /usr/local
+#
+# 	For more information on a system-wide installation, check:
+#
+# 		https://ipython.readthedocs.io/en/stable/install/kernel_install.html#kernels-for-different-environments
+#
+# 		The prefix is given as /usr/local as it seems /share/jupyter/kernels
+# 		is automatically appended
+#
+######
+#
+# This script installs the kernel/environment for one tutorial
+#
+# It is assumed the script is run from the same directory it is located in.
+#
+#######
+#
+
+# Check if the script is being run from ../install_environments.sh
+# or from current directory
+
+if [ -z "${INSTALL_ROOT_DIR}" ]; then
+
+		# Running from current directory
+
+		T_PATH=$PWD
+
+		# Parse input arguments
+
+		OPTIND=0
+		JUPYTER_PREFIX=""
+
+		while getopts "h?p:" opt; do
+		    case "$opt" in
+		    h|\?)
+		        echo "Usage: [sudo] $0 [-p /path/to/jupyter/kernels]"
+				return
+		        ;;
+		    p)  JUPYTER_PREFIX=$OPTARG
+		        ;;
+		    esac
+		done
+
+		shift "$((OPTIND-1))"
+
+		[ "${1:-}" = "--" ] && shift
+
+		# Check if installation is user-wide or system-wide
+
+		if [ -z "${JUPYTER_PREFIX}" ];
+
+			then
+				IPYKERNEL_MODE="--user"
+
+			else
+				IPYKERNEL_MODE="--prefix=$JUPYTER_PREFIX"
+
+			fi
+
+fi
 
 # Check if CONDA_PREFIX is set
 
@@ -14,7 +81,7 @@ if [ -z "${CONDA_PREFIX}" ]; then
 	echo "The CONDA_PREFIX variable is empty."
 	echo "Possibly, the conda base environment is not activated or conda is not installed."
 	echo "Exiting."
-	exit 1
+	return
 fi
 
 # Check if currently in the base environment
@@ -22,21 +89,12 @@ fi
 if [ "$CONDA_DEFAULT_ENV" != "base" ]; then
 	echo "This script should be run in the base conda environment."
 	echo "Exiting."
-	exit 1
+	return
 fi
 
 # Remember the prefix of the base environment
 
 CONDA_BASE_PREFIX=$CONDA_PREFIX
-
-# Check if the script is being run from ../install_environments.sh
-# or from current directory
-
-if [ -z "${INSTALL_ROOT_DIR}" ]; then
-
-	T_PATH=$PWD
-
-fi
 
 # Get tutorial number
 
@@ -47,7 +105,7 @@ T_NO=$(basename "$T_PATH")
 conda create -y -n $T_NO -c conda-forge -c anaconda ipykernel xtb-python
 
 source $CONDA_BASE_PREFIX/bin/activate $T_NO
-python -m ipykernel install --user --name=$T_NO
+python -m ipykernel install $IPYKERNEL_MODE --name=$T_NO
 python -m pip install -r $T_PATH/requirements.txt
 
 source $CONDA_BASE_PREFIX/bin/activate base
--- install/install_environments.sh
+++ install/install_environments.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 #
-# Usage:
+# (1) User-wide installation:
 #
-# in a conda base environment, run:
+# 	in a conda base environment, run:
 #
-# source install_environments.sh
+# 		source install_environments.sh
+#
+# (2) System-wide installation
+#
+# 		sudo -s
+#
+# 		# activate conda base environment
+# 		source /path/to/conda/activate base
+#
+# 		source install_environments.sh -p /usr/local
+#
+# 	For more information on a system-wide installation, check:
+#
+# 		https://ipython.readthedocs.io/en/stable/install/kernel_install.html#kernels-for-different-environments
 # 
+# 		The prefix is given as /usr/local as it seems /share/jupyter/kernels
+# 		is automatically appended
+#
 ######
 #
 # The aim of this script is to automatize the installation process of
@@ -22,13 +38,33 @@
 #######
 #
 
+# Parse input arguments
+
+OPTIND=0
+JUPYTER_PREFIX=""
+
+while getopts "h?p:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Usage: [sudo] $0 [-p /path/to/jupyter/kernels]"
+		return
+        ;;
+    p)  JUPYTER_PREFIX=$OPTARG
+        ;;
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+[ "${1:-}" = "--" ] && shift
+
 # Check if CONDA_PREFIX is set
 
 if [ -z "${CONDA_PREFIX}" ]; then
 	echo "The CONDA_PREFIX variable is empty."
 	echo "Possibly, the conda base environment is not activated or conda is not installed."
 	echo "Exiting."
-	exit 1
+	return
 fi
 
 # Check if currently in the base environment
@@ -36,7 +72,7 @@ fi
 if [ "$CONDA_DEFAULT_ENV" != "base" ]; then
 	echo "This script should be run in the base conda environment."
 	echo "Exiting."
-	exit 1
+	return
 fi
 
 # Remember the prefix of the base environment
@@ -47,14 +83,26 @@ CONDA_BASE_PREFIX=$CONDA_PREFIX
 
 INSTALL_ROOT_DIR=$PWD
 
+# Check if installation is user-wide or system-wide
+
+if [ -z "${JUPYTER_PREFIX}" ];
+
+	then
+		IPYKERNEL_MODE="--user"
+
+	else
+		IPYKERNEL_MODE="--prefix=$JUPYTER_PREFIX"
+
+fi
+
 ### Install all environments and kernels
 
 for i in 2
 do
 
-	echo "----------------"
+	echo "-----------------"
 	echo "Installing T$i..."
-	echo "----------------"
+	echo "-----------------"
 
 	T_PATH="$INSTALL_ROOT_DIR/T$i"
 
